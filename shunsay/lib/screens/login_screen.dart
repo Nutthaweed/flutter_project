2c0b0bf8fdd8c32c67f95e93871ada56fdc94c92
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2024-02-07 21:51:34 +0700

    new font

--- shunsay/lib/screens/login_screen.dart
+++ shunsay/lib/screens/login_screen.dart
@@ -16,7 +16,11 @@ class LoginPage extends StatelessWidget {
           child: Column(
             children: [
               const SizedBox(height: 50),
-              const Icon(Icons.lock,size: 100),
+              const Image(
+                image: AssetImage('assets/images/Kmutnb.jpg'),
+                height: 100,
+                width:  100,
+              ),
               const SizedBox(height: 50),
               Text('Welcome back!',
               style: TextStyle(
